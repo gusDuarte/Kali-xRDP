f76565d3037966f788adad5e306ef46a69dea8ad
Gustavo Duarte <gus.duarte@gmail.com>
2021-05-26 18:47:09 -0300

    Fix checks

--- Gnome-Xserver.cmd
+++ Gnome-Xserver.cmd
@@ -17,10 +17,10 @@ SET RUNSTART=%date% @ %time:~0,5%
 
 ECHO:
 ECHO [%TIME:~0,8%] Enable Virtual Machine feature
-PowerShell.exe -Command "$do = Get-Content $env:TEMP\vmenable.TMP; IF ($do -ne 'done') {dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart}; ECHO 'done' > $env:TEMP\vmenable.TMP"
+PowerShell.exe -Command "IF (Test-Path $env:TEMP\vmenable.TMP) {$do = Get-Content $env:TEMP\vmenable.TMP}; IF ($do -ne 'done') {dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart}; ECHO 'done' > $env:TEMP\vmenable.TMP"
 ECHO:
 ECHO [%TIME:~0,8%] Download and Install Linux Kernel Update
-PowerShell.exe -Command "$do = Get-Content $env:TEMP\kernelupdate.TMP ; IF ($do -ne 'done') {Start-BitsTransfer -Source https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi -Destination $env:TEMP\wsl_update_x64.msi ; $env:TEMP\wsl_update_x64.msi; ECHO 'done' > $env:TEMP\kernelupdate.TMP}"
+PowerShell.exe -Command "IF (Test-Path $env:TEMP\kernelupdate.TMP) {$do = Get-Content $env:TEMP\kernelupdate.TMP}; IF ($do -ne 'done') {Start-BitsTransfer -Source https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi -Destination $env:TEMP\wsl_update_x64.msi ; $env:TEMP\wsl_update_x64.msi; ECHO 'done' > $env:TEMP\kernelupdate.TMP}"
 
 ECHO:
 ECHO [%TIME:~0,8%] Set WSL version 2.
