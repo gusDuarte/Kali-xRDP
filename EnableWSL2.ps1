ec5d531faa686cb84d72e1ba5d09021843389409
Gustavo Duarte <gus.duarte@gmail.com>
2021-05-29 20:45:39 -0300

    Fix enable wsl2.

--- EnableWSL2.ps1
+++ EnableWSL2.ps1
@@ -8,7 +8,8 @@ if ($WSL.State -eq 'Disabled')
 }
 
 Write-Output "Check if Virtual Machine feature is enabled, it not, enable and RESTART !!!"
-IF (Test-Path $env:TEMP\vmenable.TMP)
+$r = Test-Path $env:TEMP\vmenable.TMP
+IF ($r -eq $false)
 {
   dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart;
   Write-Output 'done' > $env:TEMP\vmenable.TMP; Restart-Computer;
@@ -17,7 +18,8 @@ IF (Test-Path $env:TEMP\vmenable.TMP)
 }
 
 Write-Output "Download and Install Linux Kernel Update"
-IF (Test-Path $env:TEMP\kernelupdate.TMP)
+$r = Test-Path $env:TEMP\kernelupdate.TMP
+IF ($r -eq $false)
 {
   Start-BitsTransfer -Source https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi -Destination $env:TEMP\wsl_update_x64.msi ;
   Start-Process $env:TEMP\wsl_update_x64.msi -ArgumentList '/quiet /passive';
