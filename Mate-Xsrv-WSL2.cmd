e351ff58a4e61c581085d04a5cf4062428b0ab5f
Gustavo Duarte <gus.duarte@gmail.com>
2021-05-29 10:44:56 -0300

    Split script to enable WSL2.

--- Mate-Xsrv-WSL2.cmd
+++ Mate-Xsrv-WSL2.cmd
@@ -11,21 +11,13 @@ ECHO [Ubuntu Gnome-Xserver Installer 20210521]
 ECHO:
 ECHO:
 
-ECHO [%TIME:~0,8%] Check if WSL is enabled, if not, enable and RESTART !!!.
-PowerShell.exe -Command "$WSL = Get-WindowsOptionalFeature -Online -FeatureName 'Microsoft-Windows-Subsystem-Linux' ; if ($WSL.State -eq 'Disabled') {dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart; Restart-Computer }"
-SET RUNSTART=%date% @ %time:~0,5%
+REM ##
+REM ## Enable WSL2
+REM ##
+PowerShell.exe -ExecutionPolicy bypass -command "Start-BitsTransfer -Source '%BASE%/EnableWSL2.ps1' -Destination $env:TEMP\EnableWSL2.ps1;"
+PowerShell.exe -ExecutionPolicy bypass -command "$env:TEMP\EnableWSL2.ps1"
 
-ECHO:
-ECHO [%TIME:~0,8%] Check if Virtual Machine feature is enabled, it not, enable and RESTART !!!
-PowerShell.exe -Command "IF (Test-Path $env:TEMP\vmenable.TMP) {$do = Get-Content $env:TEMP\vmenable.TMP} ELSE {$do = 'no'}; IF ($do -ne 'done') {dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart}; ECHO 'done' > $env:TEMP\vmenable.TMP; Restart-Computer"
 
-ECHO:
-ECHO [%TIME:~0,8%] Download and Install Linux Kernel Update
-PowerShell.exe -Command "IF (Test-Path $env:TEMP\kernelupdate.TMP) {$do = Get-Content $env:TEMP\kernelupdate.TMP} ELSE {$do = 'no'}; IF ($do -ne 'done') {Start-BitsTransfer -Source https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi -Destination $env:TEMP\wsl_update_x64.msi ; Start-Process $env:TEMP\wsl_update_x64.msi -ArgumentList '/quiet /passive'; ECHO 'done' > $env:TEMP\kernelupdate.TMP}"
-
-ECHO:
-ECHO [%TIME:~0,8%] Set WSL version 2.
-PowerShell.exe -Command  "wsl --set-default-version 2"
 
 
 ECHO:
